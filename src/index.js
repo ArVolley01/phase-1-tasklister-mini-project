aab7d77059c2ab5d87795298d9095fffdc12d7e5
Ash Dan <Arvolley01@gmail.com>
2022-06-29 09:58:37 -0400

    added delete button for tasks

--- src/index.js
+++ src/index.js
@@ -8,8 +8,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const text = document.getElementById("new-task-description");
     const task = document.createElement('li');
     task.textContent = text.value;
+    task.append(getDeleteButton(task));
     document.getElementById("tasks").append(task);
   });
 
-
 });
+
+function getDeleteButton(task) {
+  let button = document.createElement('button');
+  button.addEventListener('click', () => {
+    task.remove();
+  })
+  button.textContent = "DELETE";
+  return button;
+}
