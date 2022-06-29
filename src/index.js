255258e0b1905c3e5e268589ee0d55921033663d
Ash Dan <Arvolley01@gmail.com>
2022-06-29 09:25:48 -0400

    prevented default, added list element to task list with description text

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const subForm = document.getElementById("create-task-form");
+  subForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    alert('here');
+  }, false);
+
+  subForm.addEventListener('submit', () => {
+    const text = document.getElementById("new-task-description");
+    const task = document.createElement('li');
+    task.textContent = text.value;
+    document.getElementById("tasks").append(task);
+  });
+
+
 });
