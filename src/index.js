9cf77e6e5f05098471c47f87b2661b0fa7be0ad5
Ash Dan <Arvolley01@gmail.com>
2022-06-29 11:02:52 -0400

    implemented priorities

--- src/index.js
+++ src/index.js
@@ -6,13 +6,19 @@ document.addEventListener("DOMContentLoaded", () => {
     event.preventDefault();
   }, false);
 
+  priorities.map((priority) => {
+    const marker = document.createElement('div');
+    marker.id = priority;
+    document.getElementById('tasks').append(marker);
+  });
+
   subForm.addEventListener('submit', () => {
     const text = document.getElementById("new-task-description");
     const task = document.createElement('li');
     task.textContent = text.value;
-    task.append(getDeleteButton(task));
     task.append(getDropdown(task));
-    document.getElementById("tasks").append(task);
+    task.append(getDeleteButton(task));
+    document.getElementById(priorities[0]).append(task);
   });
 
 });
@@ -29,6 +35,9 @@ function getDeleteButton(task) {
 function getDropdown(task) {
   const menu = document.createElement('form');
   const selection = document.createElement('select');
+  selection.addEventListener('input', (e) => {
+    document.getElementById(e.target.value).append(task);
+  });
   priorities.map((priority) => {
     const opt = document.createElement('option');
     opt.setAttribute('value', priority);
